7875ea7e50b312acb0219f9fc47502b99b66cc4b
modolee <modolee.si@gmail.com>
2023-10-24 14:21:22 +0900

    login 페이지 수정 테스트

--- front/src/App.js
+++ front/src/App.js
@@ -2,32 +2,26 @@ import React from 'react';
 import './styles/style.css';
 import Layout from './layouts/Layout';
 
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/main';
 import Product from './pages/products/productView';
 // import Product from './Product'; <Main>여기에 상품 목록 뿌려주기</Main>
-
-
-
+// Hello World!!!
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
-      <Layout>
-
-        <Routes>
-					<Route path="/" element={<Main />}></Route>
-          <Route path="/products/:productId" element={<Product />}></Route>
-        </Routes>
-			
-        
-      </Layout>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Main />}></Route>
+            <Route path='/products/:productId' element={<Product />}></Route>
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
