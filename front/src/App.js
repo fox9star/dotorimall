f2589ce6e030a4f3d7571aea67b964ac32f42715
future4ing <mirae4120@gmail.com>
2023-10-26 13:57:06 +0900

    fix : 로그인 페이지 전환 방식 변경
    - App.jsx : 로그인 페이지 전환 방식으로 route 사용
    - header.jsx : 연결 방식 라우터 Link 사용

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import './styles/style.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Main from './pages/main';
 import Product from './pages/products/productView';
 import Login from './pages/login/login';
 
-// import Product from './Product'; <Main>여기에 상품 목록 뿌려주기</Main>
-
-
-
-
 function App() {
-  
-
   return (
-    <div className="App">
-
+    <div className='App'>
       <BrowserRouter>
-        <Routes>
+          <Routes>
             <Route path="/login/login" element={<Login />}></Route>
-            <Route path="/" element={<Main />}></Route>
-            <Route path="/products/:productId" element={<Product />}></Route>
-
-        </Routes>
+            <Route path='/' element={<Main />}></Route>
+            <Route path='/products/:productId' element={<Product />}></Route>
+          </Routes>
       </BrowserRouter>
-
-			
     </div>
-  )
+  );
 }
 
 export default App;
